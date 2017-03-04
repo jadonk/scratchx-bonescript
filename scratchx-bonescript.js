92421b3bc926ced60e1927df7b88fdd0e7ef2813
Jason Kridner <github@hangerhead.com>
2017-03-03 22:43:58 -0600

    Update scratchx-bonescript.js

--- scratchx-bonescript.js
+++ scratchx-bonescript.js
@@ -1,19 +1,26 @@
 (function(ext) {
+    var boardStatus = 0; // 0 = not ready, 2 = ready
+    
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+        return {status: boardStatus, msg: 'Ready'};
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-        ]
+            ['h', 'when %m.button button %m.buttonAction', 'onButton'],
+        ],
+        menus: {
+            button: ['pause', 'mode'],
+            buttonAction: ['pressed', 'released'],
+        }
     };
 
     // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
+    ScratchExtensions.register('BoneScript', descriptor, ext);
 })({});
